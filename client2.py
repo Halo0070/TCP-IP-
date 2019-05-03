689bd1946f5f24cb9cb62ee57b34b236229bd9f4
starcraft1 <lee998598@gmail.com>
2019-05-03 22:30:38 +0900

    Update client2.py

---
+++ client2.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('{0} <Bind IP> <Server IP> <Message>'.format(sys.argv[0]))
+        sys.exit()
+
+    bindIP = sys.argv[1]
+    serverIP = sys.argv[2]
+    message = sys.argv[3]
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # SOCK_STREAM은 TCP socket을 뜻함
+    sock.bind((bindIP, 0))
