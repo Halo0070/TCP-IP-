8e7dd4497646c178e2ab692d32dd0816b75b66be
starcraft1 <lee998598@gmail.com>
2019-05-05 22:14:51 +0900

    Update socket2.py

--- socket2.py
+++ socket2.py
@@ -9,11 +9,11 @@ class TCP(socketserver.BaseRequestHandler):
          buffer=sock.recv(1024)
          received = str(buffer, encoding='utf-8')
 
-         print('수신 : {0}'.format(received))
+         print('receive : {0}'.format(received))
 
         # 수신한 데이터를 그대로 돌려보냄.
          sock.send(buffer)  # 수신한 데이터를 그대로 클라이언트로 다시 송신.
-         print('송신 : {0} '.format(received))
+         print('transmission : {0} '.format(received))
          sock.close()
 
 if __name__ == '__main__':
